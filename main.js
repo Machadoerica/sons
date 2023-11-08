33261f21a4924e9c7bbefb23d798db5643bd6e6d
Machadoerica <144906356+Machadoerica@users.noreply.github.com>
2023-11-08 09:03:06 -0300

    ok

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 let contador = 0;
-while(contador < listaDeTecla.length ){
+while(contador < listaDeTecla.length){
     const efeito = listaDeTecla[contador].classList[1];
     const idAudio = "#som_"+efeito;
     listaDeTecla[contador].onclick = function(){
